a859858fa3f9c81f8d80d9d367ead21ec41bb38b
GuiGranville <guigra13@gmail.com>
2022-05-18 15:48:17 -0300

    Excluindo filmes e criando aviso personalizado

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,10 +1,12 @@
 import {useEffect, useState} from 'react';
-import {useParams} from 'react-router-dom';
+import {useParams, useNavigate} from 'react-router-dom';
 import api from '../../services/api';
 import './filme-info.css';
+import { toast } from 'react-toastify';
 
 function Filme(){
     const {id} = useParams();
+    const navigate = useNavigate();
     const [filme, setFilme] = useState({});
     const [loading, setLoading] = useState(true);
 
@@ -22,6 +24,8 @@ function Filme(){
             })
             .catch(() =>{
                 console.log('Filme não encontrado');
+                navigate("/", {replace: true})
+                return;
             })
         }
         loadFilme();
@@ -30,7 +34,24 @@ function Filme(){
         return() => {
             console.log("COMPONENTE FOI DESMONTADO");
         }
-    }, [])
+    }, [navigate, id])
+
+    function salvarFilme(){
+        const minhaLista = localStorage.getItem('@primeFlix');
+
+        let filmesSalvos = JSON.parse(minhaLista) || [];
+
+        const hasFilme = filmesSalvos.some((filmesSalvo) => filmesSalvo.id === filme.id)
+        
+        if(hasFilme){
+            toast.warn("Esse filme já está salvo")
+            return;
+        }
+
+        filmesSalvos.push(filme);
+        localStorage.setItem("@primeFlix", JSON.stringify(filmesSalvos));
+        toast.success("Filme salvo com sucesso")
+    }
 
     if(loading){
         return(
@@ -49,9 +70,9 @@ function Filme(){
             <strong>Avaliação: {filme.vote_average} / 10</strong>
 
             <div className='area-buttons'>
-                <button>Salvar</button>
+                <button onClick={salvarFilme}>Salvar</button>
                 <button>
-                    <a href='#'>
+                    <a href={`https://youtube.com/results?search_query=${filme.title} trailer`} target='_blank'>
                         Trailer
                     </a>
                 </button>
