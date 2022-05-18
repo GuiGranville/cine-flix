d522f661b0480e52588803b9a8e382295374aeec
GuiGranville <guigra13@gmail.com>
2022-05-18 11:29:26 -0300

    Listando  filmes

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,7 +1,61 @@
+import {useEffect, useState} from 'react';
+import {useParams} from 'react-router-dom';
+import api from '../../services/api';
+import './filme-info.css';
+
 function Filme(){
+    const {id} = useParams();
+    const [filme, setFilme] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    useEffect(()=>{
+        async function loadFilme(){
+            await api.get(`/movie/${id}`,{
+                params:{
+                    api_key: 'edc53a3b26a266f68da217aeb3d89b71',
+                    language: "pt-BR",
+                }
+            })
+            .then((response)=> {
+                setFilme(response.data);
+                setLoading(false);
+            })
+            .catch(() =>{
+                console.log('Filme não encontrado');
+            })
+        }
+        loadFilme();
+
+
+        return() => {
+            console.log("COMPONENTE FOI DESMONTADO");
+        }
+    }, [])
+
+    if(loading){
+        return(
+            <div className='filme-info'>
+                <h1>Carrgando detalhes...</h1>
+            </div>
+        )
+    }
     return(
-        <div>
-            <h1>Bem vindo a pagina de detalhes do filme</h1>
+        <div className='filme-info'>
+            <h1>{filme.title}</h1>
+            <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`}  alt={filme.title}/>
+        
+            <h3>Sinopse</h3>
+            <span>{filme.overview}</span>
+            <strong>Avaliação: {filme.vote_average} / 10</strong>
+
+            <div className='area-buttons'>
+                <button>Salvar</button>
+                <button>
+                    <a href='#'>
+                        Trailer
+                    </a>
+                </button>
+            </div>
         </div>
     )
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,13 @@
 import {useEffect, useState} from 'react';
 import api from '../../services/api';
+import {Link} from 'react-router-dom';
+import './home.css';
 
 // /movie/now_playing?api_key=edc53a3b26a266f68da217aeb3d89b71&language=pt-BR
 
 function Home(){
     const [filmes, setFilmes] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         async function loadFilmes(){
@@ -16,13 +19,33 @@ function Home(){
                 }
             })
             
-            console.log(response.data.results);
+            //console.log(response.data.results.slice(0, 10));
+            setFilmes(response.data.results.slice(0, 10));
+            setLoading(false);
         }
         loadFilmes();
     }, [])
+
+    if(loading){
+        return(
+            <div className='loading'>
+                <h2>Carregando filmes...</h2>
+            </div>
+        )
+    }
     return(
-        <div>
-            <h1>Bem vindo a home</h1>
+        <div className='container'>
+            <div className='lista-filmes'>
+                {filmes.map((filme) =>{
+                    return(
+                        <article key={filme.id}>
+                            <strong>{filme.title}</strong>
+                            <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title}/>
+                            <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                        </article>
+                    )
+                })}
+            </div>
         </div>
     )
 }
